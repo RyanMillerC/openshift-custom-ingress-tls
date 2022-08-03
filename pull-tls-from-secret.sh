d1eb6d81fff84d542da9ea8524c50188620ce875
Ryan Miller <miller@redhat.com>
2022-08-03 16:14:41 -0400

    Update cert export script

--- pull-tls-from-secret.sh
+++ pull-tls-from-secret.sh
@@ -7,37 +7,52 @@
 # There is no validation to make sure your certs are in the right order,
 # that's on you. If you're using cert-manager to pull from Let's Encrypt,
 # your certificate chain is in the right order.
+#
 
 set -e
 
 TLS_SECRET_NAME="$1"
-OUTPUT_DIR="./tls"
+TLS_SECRET_NAMESPACE='certs'
+
+# !! OUTPUT_DIR will be removed if it exists !!
+OUTPUT_DIR="/home/ryan/tls/$TLS_SECRET_NAME"
+
+CERT_PATH="${OUTPUT_DIR}/${TLS_SECRET_NAME}.crt"
+KEY_PATH="${OUTPUT_DIR}/${TLS_SECRET_NAME}.key"
+CA_PATH="${OUTPUT_DIR}/${TLS_SECRET_NAME}.ca"
+
+# Validate secret exists
+kubectl get secret "$TLS_SECRET_NAME" -n "$TLS_SECRET_NAMESPACE" || exit 1
+
+[[ -d "$OUTPUT_DIR" ]] && rm -rf "$OUTPUT_DIR"
+mkdir "$OUTPUT_DIR"
 
 # Pull cert/key
-[[ -d "$OUTPUT_DIR" ]] || mkdir "$OUTPUT_DIR"
-oc get secret "$TLS_SECRET_NAME" \
+kubectl get secret "$TLS_SECRET_NAME" \
+    -n "$TLS_SECRET_NAMESPACE" \
     -o jsonpath='{.data.tls\.crt}' \
-    | base64 -d > "$OUTPUT_DIR/tls.crt"
-oc get secret "$TLS_SECRET_NAME" \
+    | base64 -d > "$CERT_PATH"
+kubectl get secret "$TLS_SECRET_NAME" \
+    -n "$TLS_SECRET_NAMESPACE" \
     -o jsonpath='{.data.tls\.key}' \
-    | base64 -d > "$OUTPUT_DIR/tls.key"
+    | base64 -d > "$KEY_PATH"
 
 # Extract root CA - This is a hack, can't figure out a way with openssl :(
-ROOT_CA_START_LINE=$(grep -n 'BEGIN CERTIFICATE' "$OUTPUT_DIR/tls.crt" \
+ROOT_CA_START_LINE=$(grep -n 'BEGIN CERTIFICATE' "$CERT_PATH" \
                         | cut -f 1 -d ':' | tail -n 1)
 sed -n "$ROOT_CA_START_LINE,\$p" \
-    "$OUTPUT_DIR/tls.crt" > "$OUTPUT_DIR/ca.crt"
+    "$CERT_PATH" > "$CA_PATH"
 
 # Validate cert/key
-KEY_VALIDATION_HASH=$(openssl rsa -in "./$OUTPUT_DIR/tls.key" -noout -modulus | openssl sha256)
-CERT_VALIDATION_HASH=$(openssl x509 -in "./$OUTPUT_DIR/tls.crt" -noout -modulus | openssl sha256)
+KEY_VALIDATION_HASH=$(openssl rsa -in "$KEY_PATH" -noout -modulus | openssl sha256)
+CERT_VALIDATION_HASH=$(openssl x509 -in "$CERT_PATH" -noout -modulus | openssl sha256)
 if [[ $CERT_VALIDATION_HASH = $KEY_VALIDATION_HASH ]] ; then
     echo "Cert/key modulus hashes match: $CERT_VALIDATION_HASH"
 else
-    >&2 echo "Cert/key modulus hashes DO NOT MATCH!"
-    >&2 echo "./$OUTPUT_DIR/tls.crt: $CERT_VALIDATION_HASH"
-    >&2 echo "./$OUTPUT_DIR/tls.key: $KEY_VALIDATION_HASH"
+    >&2 echo 'Cert/key modulus hashes DO NOT MATCH!'
+    >&2 echo "$CERT_PATH: $CERT_VALIDATION_HASH"
+    >&2 echo "$KEY_PATH: $KEY_VALIDATION_HASH"
 fi
 
 # Print begin/end dates
-openssl x509 -in "./$OUTPUT_DIR/tls.crt" -noout -dates
+openssl x509 -in "$CERT_PATH" -noout -dates
